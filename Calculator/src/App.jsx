67838a1c618ce6c74c9f2be3210835b404b66987
raginimegala <ragini.megala@gmail.com>
2026-06-23 08:07:12 +0530

    Frontend Apps

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -9,13 +9,31 @@ function App() {
   })
   const[calc,setCalc] = useState(0)
   const[operation,setOperation] = useState("+")
+  const[error,setError] = useState({})
 
   const handleInput =(e) =>{
     const{name,value} = e.target;
     setForm((prev)=>({...prev,[name]:value}))
   }
 
+  const validate = ()=>{
+    let err={}
+
+    if(!form.num1){
+      err.num1 ="num1 required"
+    }
+    if(!form.num2){
+      err.num2="num2 required"
+    }
+    setError(err)
+    return Object.keys(err).length === 0
+  }
+
   const handleCalculate =()=>{
+
+    if(!validate()){
+      return
+    }
     const n1 = Number(form.num1)
     const n2 = Number(form.num2)
 
@@ -51,7 +69,9 @@ function App() {
           value={form.num1}
           name="num1"
           onChange={handleInput}
+          style={{border:error.num1?"2px solid red":"1px solid black"}}
         />
+        {error.num1 && <p style={{color:"red"}}>{error.num1}</p>}
       </div>
       <div>
         <label>Number 2: </label>
@@ -61,8 +81,9 @@ function App() {
           value={form.num2}
           name="num2"
           onChange={handleInput}
+          style={{border:error.num2?"2px solid red":"1px solid black"}}
         />
-
+        {error.num2 && <p style={{color:"red"}}>{error.num2}</p>}
         <select onChange={handleChange} value={operation}>
           <option value="+">+</option>
           <option value="-">-</option>
